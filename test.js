ea242050dff25612443c14dd0666502387e4f167
Md. Rishan Ahmed <166410540+Rishan757@users.noreply.github.com>
2026-02-07 09:32:08 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+
+
+const saveButton = document.querySelector(".save-btn");
+const Message   = document.querySelector(".para");
+
+// saveButton.addEventListener("click",saveUser);
+
+// function saveUser(){
+//     Message.textContent = "Registration Successfully";
+
+//     setTimeout(()=>{
+//     Message.textContent = "";
+// }, 3000);
+// }
+
+// saveButton.addEventListener("click",displayCount);
+
+// function displayCount(){
+//     let count = 1;
+//     Message.textContent = count;
+
+//     setInterval(() =>{
+//         count++;
+//         Message.textContent = count;
+//     },1000);
+
+// };
+
